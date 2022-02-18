7adf2db6dc3e7f9c46a9123dd5127aaa0a441401
ryan <zengruijiang@gmail.com>
2022-02-18 11:57:17 +1100

    first commit

---
+++ docker/app/entrypoints/app-dev-linux.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+# Create a user within the container with the required USER_UID
+# Note: Not really required for PHP FPM but it makes the container look nicer when you exec into it.
+adduser -u ${USER_UID} developer -D || true
+
+# Set the UID and GID that PHP FPM's workers should use.
+sed -i 's/^user = www-data/user = '"${USER_UID}"'/g' /usr/local/etc/php-fpm.d/www.conf
+sed -i 's/^group = www-data/group = '"${USER_GID}"'/g' /usr/local/etc/php-fpm.d/www.conf
+
+# Fix permissions of files created during build
+chown -Rf "${USER_UID}:${USER_GID}" /app/ || true
+
+# Proceed to launch PHP-FPM
+exec "php-fpm"
