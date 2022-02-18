5e82f2b0f0100556db782dd14bd040aace6d3c67
ryan <zengruijiang@gmail.com>
2022-02-18 13:18:22 +1100

    fix issue

--- docker/app/entrypoints/app.sh
+++ docker/app/entrypoints/app.sh
@@ -4,18 +4,12 @@ echo "this is app.sh"
 
 set -xe
 
-/usr/bin/composer install --prefer-dist --optimize-autoloader --no-scripts --no-progress --no-suggest;
+#/usr/bin/composer install --prefer-dist --optimize-autoloader --no-scripts --no-progress --no-suggest;
 
 # update database
 until /app/vendor/bin/sake dev/build >/dev/null 2>&1; do
 	(>&2 echo "Waiting for MySQL to be ready...")
 	sleep 1
 done
-
-
-# first arg is `-f` or `--some-option`
-if [ "${1#-}" != "$1" ]; then
-	set -- php-fpm "$@"
-fi
 
 exec "$@"
